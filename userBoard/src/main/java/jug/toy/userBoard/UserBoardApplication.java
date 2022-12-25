7db1f3b84bc4aaf130b65c4c556972975dd85e6b
Ji-Un-Gil <wldns2577@gmail.com>
2022-12-25 22:21:13 +0900

    feat: initial setup

---
+++ userBoard/src/main/java/jug/toy/userBoard/UserBoardApplication.java
@@ -0,0 +1,13 @@
+package jug.toy.userBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBoardApplication.class, args);
+	}
+
+}
